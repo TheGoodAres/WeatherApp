a40b404aaa3bbc90f14cfc7a43bb68548a354730
TheGoodAres <61661335+TheGoodAres@users.noreply.github.com>
2023-04-08 15:11:51 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Robert-Dumitru Oprea on 08/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
